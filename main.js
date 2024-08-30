03302afe00d04fb9408457798074d939e00ec482
Browser <92232900+browse999@users.noreply.github.com>
2024-08-30 20:51:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function topla(){
+
+    let a ,b
+
+
+    a = Number(prompt('Birinci Sayıyı giriniz'))
+    b = Number(prompt('İkinci sayıyı giriniz'))
+    let toplam = Number(a+b)
+
+
+    document.write(toplam)
+}
+
+
+
+
+
+
+
